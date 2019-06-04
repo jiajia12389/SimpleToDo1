87d69424ce2ec2d2cbdae3660bdf409449f7d59a
17018552 <jiajiatey@gmail.com>
2019-06-04 15:13:36 +0800

    added context menu

--- app/src/main/java/sg/edu/rp/c346/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/simpletodo/MainActivity.java
@@ -2,6 +2,9 @@ package sg.edu.rp.c346.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,10 +19,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     EditText etTask;
-    Button btnAdd,buttonDelete,buttonClear;
+    Button btnAdd, buttonDelete, buttonClear;
     ListView listView;
     Spinner spinner;
 
+    ArrayList taskList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +39,16 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         spinner = findViewById(R.id.spinner);
 
-        final ArrayList taskList;
-        final ArrayAdapter<String> adapter;
+
         taskList = new ArrayList<>();
 
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, taskList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
         listView.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
+                switch (position) {
                     case 0:
                         etTask.setHint("Type in a new task");
                         buttonDelete.setEnabled(false);
@@ -54,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                         etTask.setHint("Type in the index of the task to be removed");
                         btnAdd.setEnabled(false);
                         buttonDelete.setEnabled(true);
+                        etTask.setInputType(InputType.TYPE_CLASS_NUMBER);
                         break;
                 }
             }
@@ -71,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
                 taskList.add(task);
                 adapter.notifyDataSetChanged();
+                etTask.setText("");
 
             }
         });
@@ -78,12 +84,12 @@ public class MainActivity extends AppCompatActivity {
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(taskList.size()<0||etTask.getText()!=null){
+                if (taskList.size() < 0 || etTask.getText() != null) {
                     taskList.clear();
                     etTask.setText("");
                     adapter.notifyDataSetChanged();
                 }
-                if(taskList.isEmpty()){
+                if (taskList.isEmpty()) {
                     Toast.makeText(MainActivity.this, "You don't have any task to remove", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -92,21 +98,39 @@ public class MainActivity extends AppCompatActivity {
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
+                try {
                     int index = Integer.parseInt(etTask.getText().toString());
                     taskList.remove(index);
                     adapter.notifyDataSetChanged();
 
-                }catch(IndexOutOfBoundsException e){
+                } catch (IndexOutOfBoundsException e) {
                     Toast.makeText(MainActivity.this, "Wrong Index Number", Toast.LENGTH_SHORT).show();
                 }
-
-
 
             }
         });
 
 
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.delete:
+                taskList.remove(info.position);
+                adapter.notifyDataSetChanged();
+        }
+        return super.onContextItemSelected(item);
     }
 }
